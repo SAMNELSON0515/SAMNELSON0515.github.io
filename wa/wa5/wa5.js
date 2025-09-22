72adc7e4a47c2eb52d0ed42776bed41d5541b1ca
SAMNELSON0515 <samcourtrightnelson@gmail.com>
2025-09-22 08:33:20 -0600

    km

--- wa/wa5/wa5.js
+++ wa/wa5/wa5.js
@@ -1,9 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 
-navToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
 
-  const expanded = navToggle.getAttribute('aria-expanded') === 'true';
-  navToggle.setAttribute('aria-expanded', String(!expanded));
-});
+navToggle.addEventListener('click', showMenu);
+
+function showMenu() {
+  navMenu.classList.toggle ("show");
+}
+
+  
+
